9ee56196fd4aa546eeef4d3cc9b4ffb79a503c66
Tomas Savigliano <ralusek@gmail.com>
2021-01-05 17:40:16 -0700

    changed return type signature to take resolution type from promise.

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
 type TimeoutId = ReturnType<typeof setTimeout>;
 
-export default function debouncie<T extends (...args: any) => any>(
+type Resolution<T> = T extends PromiseLike<infer U> ? U : T;
+
+export default function debouncie<T extends (...args: any) => any, R extends Resolution<ReturnType<T>>>(
   fn: T,
   { debounce }: { debounce: number }
-): (...args: Parameters<T>) => Promise<ReturnType<T>> {
+): (...args: Parameters<T>) => Promise<R> {
   let activePromise: {
-    promise: Promise<ReturnType<T>>;
-    resolve: (value: ReturnType<T>) => void;
+    promise: Promise<R>;
+    resolve: (value: R) => void;
   } | null = null;
 
   let timeout: TimeoutId;
   return (...args: any) => {
     if (!activePromise) {
       let deferred: any;
-      const promise = new Promise<ReturnType<T>>((resolve) => {
+      const promise = new Promise<R>((resolve) => {
         deferred = resolve;
       });
       activePromise = {
